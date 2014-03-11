445da4bdf75d3006ceeb4e3b25ede7bcf60f7d0f
Matti Luukkainen <mluukkai@iki.fi>
2014-03-11 18:32:48 +0200

    koodit viikolle 2

--- viikko2/Ohtu-NhlStatistics1/src/main/java/ohtuesimerkki/Statistics.java
+++ viikko2/Ohtu-NhlStatistics1/src/main/java/ohtuesimerkki/Statistics.java
@@ -10,7 +10,7 @@ public class Statistics {
     private List<Player> players;
 
     public Statistics() {
-        PlayerReader reader = new PlayerReader("http://nhlstatistics.herokuapp.com/players.txt");
+        PlayerReader reader = new PlayerReader("http://nhlstats-2013-14.herokuapp.com/players.txt");
         players = reader.getPlayers();       
     }
 
