da0e532fcaad788c42daf9daffcd8f577abac808
Maria Efimvoa <maria.efimova93@gmail.com>
2014-03-19 15:15:36 +0200

    verkkokauppaa

--- viikko2/Verkkokauppa1/src/main/java/ohtu/verkkokauppa/Varasto.java
+++ viikko2/Verkkokauppa1/src/main/java/ohtu/verkkokauppa/Varasto.java
@@ -3,22 +3,23 @@ package ohtu.verkkokauppa;
 import java.util.*;
 
 public class Varasto {
-
-    private static Varasto instanssi;
-
-    public static Varasto getInstance() {
-        if (instanssi == null) {
-            instanssi = new Varasto();
-        }
-
-        return instanssi;
-    }
+//
+//    private static Varasto instanssi;
+//
+//    public static Varasto getInstance() {
+//        if (instanssi == null) {
+//            instanssi = new Varasto();
+//        }
+//
+//        return instanssi;
+//    }
 
     private Kirjanpito kirjanpito;
     private HashMap<Tuote, Integer> saldot;
 
-    private Varasto() {
-        kirjanpito = Kirjanpito.getInstance();
+    public Varasto(Kirjanpito kirjanpito) {
+//        kirjanpito = Kirjanpito.getInstance();
+        this.kirjanpito = kirjanpito;
         saldot = new HashMap<Tuote, Integer>();
         alustaTuotteet();
     }
