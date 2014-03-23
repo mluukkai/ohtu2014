30dceadce89d21171155329fc2ca2abd14b95003
unknown <eemi.haukkala@gmail.com>
2014-03-23 23:52:17 +0200

    spring osa1

--- viikko2/Verkkokauppa1/src/main/java/ohtu/verkkokauppa/Pankki.java
+++ viikko2/Verkkokauppa1/src/main/java/ohtu/verkkokauppa/Pankki.java
@@ -1,22 +1,15 @@
 package ohtu.verkkokauppa;
 
-public class Pankki {
+public class Pankki implements IPankki {
 
-    private static Pankki instanssi;
 
-    public static Pankki getInstance() {
-        if (instanssi == null) {
-            instanssi = new Pankki();
-        }
-
-        return instanssi;
-    }
     private Kirjanpito kirjanpito;
 
-    private Pankki() {
-        kirjanpito = Kirjanpito.getInstance();
+    public Pankki(IKirjanpito k) {
+        kirjanpito = (Kirjanpito) k;
     }
 
+    @Override
     public boolean tilisiirto(String nimi, int viitenumero, String tililta, String tilille, int summa) {
         kirjanpito.lisaaTapahtuma("tilisiirto: tililtä " + tilille + " tilille " + tilille
                 + " viite " + viitenumero + " summa " + summa + "e");
