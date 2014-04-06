50a1a407428ccbba6ecb8b52a97b38dffe485a1f
Juhani Helio <juhani.helio@helsinki.fi>
2014-04-06 23:31:47 +0300

    Viikko4Palautus

---
+++ HeliöJuhani/viikko4/Verkkokauppa1/src/main/java/ohtu/verkkokauppa/Pankki.java
@@ -0,0 +1,19 @@
+package ohtu.verkkokauppa;
+
+public class Pankki implements PankkiInterface {
+
+    private Kirjanpito kirjanpito;
+
+    public Pankki(Kirjanpito k) {
+        kirjanpito = k;
+    }
+
+    @Override
+    public boolean tilisiirto(String nimi, int viitenumero, String tililta, String tilille, int summa) {
+        kirjanpito.lisaaTapahtuma("tilisiirto: tililtä " + tilille + " tilille " + tilille
+                + " viite " + viitenumero + " summa " + summa + "e");
+
+        // täällä olisi koodi joka ottaa yhteyden pankin verkkorajapintaan
+        return true;
+    }
+}
