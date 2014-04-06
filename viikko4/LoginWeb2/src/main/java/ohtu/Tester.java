5761b9cc697cb32e50893c20b3bdda4a397340e7
Santeri Martikainen <santerim@cs.helsinki.fi>
2014-04-06 23:27:21 +0300

    vk4 t8

--- viikko4/LoginWeb2/src/main/java/ohtu/Tester.java
+++ viikko4/LoginWeb2/src/main/java/ohtu/Tester.java
@@ -10,22 +10,68 @@ public class Tester {
         WebDriver driver = new HtmlUnitDriver();
 
         driver.get("http://localhost:8090");
+        System.out.println("<<<<<<<<<<<<< Aloitussivu >>>>>>>>>>>>>>");
         System.out.println( driver.getPageSource() );
         WebElement element = driver.findElement(By.linkText("login"));       
         element.click(); 
         
-        System.out.println("==");
+        System.out.println("<<<<<<<<<<<<<<< Loginin-sivu >>>>>>>>>>>>>>");
         
         System.out.println( driver.getPageSource() );
         element = driver.findElement(By.name("username"));
         element.sendKeys("pekka");
         element = driver.findElement(By.name("password"));
+        element.sendKeys("pekka");
+        element = driver.findElement(By.name("login"));
+        element.submit();
+        
+        System.out.println("<<<<<<<<<<<<<<<< Väärällä salasanalla >>>>>>>>>>>>>>>>");
+        System.out.println( driver.getPageSource() );
+        
+        element = driver.findElement(By.name("username"));
+        element.sendKeys("akkep");
+        element = driver.findElement(By.name("password"));
         element.sendKeys("akkep");
         element = driver.findElement(By.name("login"));
         element.submit();
         
-        System.out.println("==");
-        System.out.println( driver.getPageSource() );
+        System.out.println("<<<<<<<<<<<<<<<< Väärällä loginilla >>>>>>>>>>>>>>>>>");
+        System.out.println(driver.getPageSource());
         
+        element = driver.findElement(By.linkText("back to home"));
+        element.click();
+        
+        System.out.println("<<<<<<<<<<<<<<<< Back to home >>>>>>>>>>>>>>>");
+        System.out.println(driver.getPageSource());
+        
+        element = driver.findElement(By.linkText("register new user"));
+        element.click();
+        
+        System.out.println("<<<<<<<<<<<<<<<< Register new user >>>>>>>>>>>>>");
+        System.out.println(driver.getPageSource());
+        
+        element = driver.findElement(By.name("username"));
+        element.sendKeys("nyyrikki");
+        element = driver.findElement(By.name("password"));
+        element.sendKeys("nyyrikki1");
+        element = driver.findElement(By.name("passwordConfirmation"));
+        element.sendKeys("nyyrikki1");
+        element = driver.findElement(By.name("add"));
+        element.click();
+        
+        System.out.println("<<<<<<<<<<<<<<< New user >>>>>>>>>>>>>>>>>>");
+        System.out.println(driver.getPageSource());
+        
+        element = driver.findElement(By.linkText("continue to application mainpage"));
+        element.click();
+        
+        System.out.println("<<<<<<<<<<<<<< Application mainpage >>>>>>>>>>>>");
+        System.out.println(driver.getPageSource());
+        
+        element = driver.findElement(By.linkText("logout"));
+        element.click();
+        
+        System.out.println("<<<<<<<<<<<<<<<< Uloskirjauksen jälkeen >>>>>>>>>>>>>");
+        System.out.println(driver.getPageSource());
     }
 }
