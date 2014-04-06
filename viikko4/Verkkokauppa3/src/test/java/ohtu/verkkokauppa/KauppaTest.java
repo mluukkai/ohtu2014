39fdf15a6beaa193501153d8ec032c506d57b9fd
Ilkka <ilkka.lehikoinen@helsinki.fi>
2014-04-06 09:57:11 +0300

    viikko 4 3 tehtävän jälkeen

--- viikko4/Verkkokauppa3/src/test/java/ohtu/verkkokauppa/KauppaTest.java
+++ viikko4/Verkkokauppa3/src/test/java/ohtu/verkkokauppa/KauppaTest.java
@@ -222,4 +222,33 @@ public class KauppaTest {
         
     }   
   
+  @Test
+    public void tuotePoistetaanOstoskorista() {
+        // luodaan ensin mock-oliot
+        Pankki pankki = mock(Pankki.class);
+
+        Viitegeneraattori viite = mock(Viitegeneraattori.class);
+        // määritellään että viitegeneraattori palauttaa viitten 42
+        when(viite.uusi()).thenReturn(42);
+
+        Varasto varasto = mock(Varasto.class);
+        // määritellään että tuote numero 1 on maito jonka hinta on 5 ja saldo 1
+        when(varasto.saldo(1)).thenReturn(10);        
+     //   when(varasto.saldo(2)).thenReturn(0);
+        when(varasto.haeTuote(1)).thenReturn(new Tuote(1, "maito", 5));      
+      //  when(varasto.haeTuote(2)).thenReturn(new Tuote(2, "lehmä", 12));
+
+        // sitten testattava kauppa 
+        Kauppa k = new Kauppa(varasto, pankki, viite);              
+
+        // tehdään ostokset
+        k.aloitaAsiointi();
+        k.lisaaKoriin(1);       // ostetaan tuotetta numero 1 eli maitoa
+        k.lisaaKoriin(1);       // ostetaan tuotetta numero 1 eli maitoa
+        k.poistaKorista(1);     // ostetaan tuotetta numero 1 eli maitoa
+        k.tilimaksu("jukka", "75346");    
+        
+         // sitten suoritetaan varmistus, että pankin metodia tilisiirto on kutsuttu
+        verify(pankki).tilisiirto("jukka", 42, "75346", "33333-44455", 5);               
+    }    
 }
